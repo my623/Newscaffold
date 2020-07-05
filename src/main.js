3a11f8d5dfa4953c49d40f7ef7e868ff92f7b573
luckydog623 <360841164@qq.com>
2020-07-05 23:16:00 +0800

    element和vant

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import  Axios from 'axios'
+import VueAxios from 'vue-axios'
+import store from './store'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(Vant);
+Vue.use(ElementUI);
+Vue.prototype.$axios = Axios
+Vue.use(VueAxios,Axios)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
